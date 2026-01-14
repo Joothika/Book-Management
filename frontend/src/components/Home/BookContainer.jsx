94af51e344eb78287bb6f759b06332364173fef2
r-ananth <studypurposework@gmail.com>
2026-01-14 13:01:48 +0530

    all files added

---
+++ frontend/src/components/Home/BookContainer.jsx
@@ -0,0 +1,27 @@
+import BookCard from "./BookCard"
+import { useEffect, useState } from "react"
+
+
+const BookContainer = () => {
+    const [books, setBooks] = useState([])
+    useEffect(() => {
+        const fetchAllBooks = async() => {
+            const bookData = await fetch('https://bookmanagement.development.catalystappsail.in/api/books')
+            const {data} = await bookData.json()
+            console.log(data)
+            setBooks(data)
+        };
+        fetchAllBooks();
+    },[])
+
+
+    return(
+        <div className="grid grid-cols-4 gap-10">
+            {
+                books.map(book => <BookCard key={book._id} book={book}/>)
+            }
+        </div>
+    )
+}
+
+export default BookContainer
